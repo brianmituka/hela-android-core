222093b46334fdd394214fde7ebf2ab0cc90fff6
brian <mitukab@outlook.com>
2019-11-11 22:46:22 +0300

    Added User class

--- app/src/main/java/com/creativeconsillium/drumsforafrica/helaapp/Activity/Model/User.java
+++ app/src/main/java/com/creativeconsillium/drumsforafrica/helaapp/Activity/Model/User.java
@@ -1,4 +1,40 @@
 package com.creativeconsillium.drumsforafrica.helaapp.Activity.Model;
 
 public class User {
+    public String name;
+    public int phone;
+    public String email;
+
+    public User() {
+    }
+
+    public User(String name, int phone, String email) {
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
