e776d408de4494d118a682795f88105cc2110ee1
brian <mitukab@outlook.com>
2019-11-11 22:49:13 +0300

    Added loader in UI-Utils

--- app/src/main/java/com/creativeconsillium/drumsforafrica/helaapp/Activity/utils/UiUtils.java
+++ app/src/main/java/com/creativeconsillium/drumsforafrica/helaapp/Activity/utils/UiUtils.java
@@ -1,4 +1,15 @@
 package com.creativeconsillium.drumsforafrica.helaapp.Activity.utils;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
+
 public class UiUtils {
+     static ProgressDialog progressDialog;
+
+    public static void showDialog(String text, Activity activity){
+        progressDialog = ProgressDialog.show(activity, "", text, true);
+    }
+    public static void hideDialog(){
+        progressDialog.dismiss();
+    }
 }
