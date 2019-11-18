043aa73e54be3e16d6296ee08ad17b578c63a9ef
brian <mitukab@outlook.com>
2019-11-18 20:50:01 +0300

    Added fields for the mpesa class

--- app/src/main/java/com/creativeconsillium/drumsforafrica/helaapp/Activity/Model/MpesaMessage.java
+++ app/src/main/java/com/creativeconsillium/drumsforafrica/helaapp/Activity/Model/MpesaMessage.java
@@ -1,5 +1,53 @@
 package com.creativeconsillium.drumsforafrica.helaapp.Activity.Model;
 
-public class MpesaMessage {
+import java.math.BigDecimal;
+import org.joda.time.LocalDate;
 
+public class MpesaMessage {
+private String transactionCode;
+private BigDecimal amount;
+private LocalDate date;
+private String transactionTyp;
+
+    public MpesaMessage() {
+    }
+
+    public MpesaMessage(String transactionCode, BigDecimal amount, LocalDate date, String transactionTyp) {
+        this.transactionCode = transactionCode;
+        this.amount = amount;
+        this.date = date;
+        this.transactionTyp = transactionTyp;
+    }
+
+    public String getTransactionCode() {
+        return transactionCode;
+    }
+
+    public void setTransactionCode(String transactionCode) {
+        this.transactionCode = transactionCode;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getTransactionTyp() {
+        return transactionTyp;
+    }
+
+    public void setTransactionTyp(String transactionTyp) {
+        this.transactionTyp = transactionTyp;
+    }
 }
