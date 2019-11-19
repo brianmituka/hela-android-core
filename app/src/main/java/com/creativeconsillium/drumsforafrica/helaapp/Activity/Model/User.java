d9e11a9a4dd17f3707163b63cffd539a6c9709c8
brian <mitukab@outlook.com>
2019-11-19 20:57:37 +0300

    Added firebase annotation to the User object

--- app/src/main/java/com/creativeconsillium/drumsforafrica/helaapp/Activity/Model/User.java
+++ app/src/main/java/com/creativeconsillium/drumsforafrica/helaapp/Activity/Model/User.java
@@ -1,5 +1,12 @@
 package com.creativeconsillium.drumsforafrica.helaapp.Activity.Model;
 
+import com.google.firebase.database.IgnoreExtraProperties;
+
+/**
+ * Ignore any properties that
+ * aren't mapped into the database
+ */
+@IgnoreExtraProperties
 public class User {
    public String phone;
    public String userId;
