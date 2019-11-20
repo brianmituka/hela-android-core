1a075f2fe5a4c04dd86215c83536db56fda9a82d
brian <mitukab@outlook.com>
2019-11-20 16:48:45 +0300

    Added a method for uploading new incoming Mpesa messages to firebase

--- app/src/main/java/com/creativeconsillium/drumsforafrica/helaapp/Activity/Mpesa.java
+++ app/src/main/java/com/creativeconsillium/drumsforafrica/helaapp/Activity/Mpesa.java
@@ -3,13 +3,38 @@ package com.creativeconsillium.drumsforafrica.helaapp.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.provider.Telephony;
+import android.telephony.SmsMessage;
+import android.util.Log;
+
+import com.creativeconsillium.drumsforafrica.helaapp.Activity.Model.MpesaMessage;
+import com.creativeconsillium.drumsforafrica.helaapp.Activity.utils.FirebaseUtils;
+import com.creativeconsillium.drumsforafrica.helaapp.Activity.utils.SmsUtils;
 
 public class Mpesa extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        // TODO: This method is called when the BroadcastReceiver is receiving
-        // an Intent broadcast.
-        throw new UnsupportedOperationException("Not yet implemented");
+          String TAG = Mpesa.class.getSimpleName();
+       if (intent.getAction().equals(Telephony.Sms.Intents.SMS_RECEIVED_ACTION)){
+           SmsMessage[] messages = Telephony.Sms.Intents.getMessagesFromIntent(intent);
+           for (SmsMessage mesage : messages) {
+               Log.i(TAG, "The address from the message is:: " + mesage.getOriginatingAddress());
+               if (mesage.getOriginatingAddress().equals("MPESA")){
+                   Log.i(TAG, "A new Mpesa Message has arrived");
+                   String messagebody = mesage.getMessageBody();
+                   String date = SmsUtils.extractMpesaDate(messagebody);
+                   String amount = SmsUtils.extractMpesaAmount(messagebody);
+                   String transactionCode = SmsUtils.extractMpesaCode(messagebody);
+                   String transactionType = SmsUtils.mpesaMessageType(messagebody);
+                   MpesaMessage messageToUpload = new MpesaMessage(transactionCode, amount, date, transactionType);
+                   SmsUtils.uploadMessageToFirebase(messageToUpload);
+               }else {
+                   Log.i(TAG, "It is not an mpesa message");
+               }
+
+           }
+       }
     }
 }
