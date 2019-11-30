93da312890219534a130b705575a691fe2577914
brian <mitukab@outlook.com>
2019-11-30 16:45:21 +0300

    Added extra fields in the class and added firebase

--- app/src/main/java/com/creativeconsillium/drumsforafrica/helaapp/Activity/Model/ModelBudgets.java
+++ app/src/main/java/com/creativeconsillium/drumsforafrica/helaapp/Activity/Model/ModelBudgets.java
@@ -1,5 +1,12 @@
 package com.creativeconsillium.drumsforafrica.helaapp.Activity.Model;
 
+import com.google.firebase.database.Exclude;
+import com.google.firebase.database.IgnoreExtraProperties;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@IgnoreExtraProperties
 public class ModelBudgets {
 
     private String budgetCategory;
@@ -7,8 +14,18 @@ public class ModelBudgets {
     private String budgetSpentAmount;
     private String budgetRemainingAmount;
     private String budgetType;
+    private String budgetName;
+    private boolean isRecurring;
+    private String budgetAmount;
 
     public ModelBudgets() {
+    }
+
+
+    public ModelBudgets(String budgetName, boolean isRecurring, String budgetAmount) {
+        this.budgetName = budgetName;
+        this.isRecurring = isRecurring;
+        this.budgetAmount = budgetAmount;
     }
 
     public ModelBudgets(String budgetCategory, String budgetNumber, String budgetSpentAmount, String budgetRemainingAmount) {
@@ -22,6 +39,16 @@ public class ModelBudgets {
     public String getBudgetNumber() { return budgetNumber; }
     public String getBudgetSpentAmount() { return budgetSpentAmount; }
     public String getBudgetRemainingAmount() { return budgetRemainingAmount; }
+    @Exclude
+    public Map<String, Object> toMap(){
+        HashMap<String, Object> budget = new HashMap<>();
+        budget.put("budgetName", budgetName);
+        budget.put("budgetAmount", budgetAmount);
+        budget.put("isRecurring", isRecurring);
+
+
+        return budget;
+    }
 
 
 }
