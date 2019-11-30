9b3fc15d9faf71398f8df8522b7dfb2c9408f415
brian <mitukab@outlook.com>
2019-11-30 16:47:38 +0300

    Added a class for month summaries

--- app/src/main/java/com/creativeconsillium/drumsforafrica/helaapp/Activity/Model/MonthSummary.java
+++ app/src/main/java/com/creativeconsillium/drumsforafrica/helaapp/Activity/Model/MonthSummary.java
@@ -1,4 +1,30 @@
 package com.creativeconsillium.drumsforafrica.helaapp.Activity.Model;
 
+import com.google.firebase.database.Exclude;
+import com.google.firebase.database.IgnoreExtraProperties;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@IgnoreExtraProperties
 public class MonthSummary {
+    private String amountReceived;
+    private String amountSpent;
+
+    public MonthSummary() {
+    }
+
+    public MonthSummary(String amountReceived, String amountSpent) {
+        this.amountReceived = amountReceived;
+        this.amountSpent = amountSpent;
+    }
+    @Exclude
+    public Map<String, Object> toMap(){
+        HashMap<String, Object> monthSummary = new HashMap<>();
+        monthSummary.put("monthSpent", amountSpent);
+        monthSummary.put("monthReceived", amountReceived);
+
+
+        return monthSummary;
+    }
 }
